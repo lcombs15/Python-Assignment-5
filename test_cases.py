078f9ece9b4c06f040a0679c5f65dc33b2fffa7c
Lucas Combs <l-combs@hotmail.com>
2018-03-18 20:30:30 -0400

    fixed style issues

--- test_cases.py
+++ test_cases.py
@@ -2,12 +2,14 @@ import unittest
 from creature import Hero, Monster
 import room
 
-class test_fighting(unittest.TestCase):
+
+class TestFighting(unittest.TestCase):
 
     def test_fight(self):
         weak = Monster("Blob", 2, None, None)
         strong = Hero("Lucas", 400, None, None)
         self.assertEquals(room.fight(weak,strong), strong)
+
 
 if __name__ == '__main__':
     unittest.main()
