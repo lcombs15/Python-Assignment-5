29f97899b1cc1ec73feb0e9586043fac8bdb5791
Lucas Combs <l-combs@hotmail.com>
2018-03-26 15:43:58 -0400

    Weapons can be randomly generated now.

--- weapon.py
+++ weapon.py
@@ -1,8 +1,16 @@
+import random
 class Weapon:
     def __init__(self, name, max_damage, swings_per_turn):
         self.name = name
         self.max_damage = max_damage
         self.swings_per_turn = swings_per_turn
+
+
+    def __init__(self):
+        types = ["Knife", "Sword", "Bow & Arrow", "Spiked Baseball Bat", "Sock o' butter", "Large pipe"]
+        self.name = random.randint(0,types.__len__())
+        self.max_damage = random.randint(1, 10)
+        self.swings_per_turn = random.randint(1,5)
 
     # String override for printing
     def __str__(self):
