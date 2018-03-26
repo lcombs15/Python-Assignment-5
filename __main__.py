617afb25eb3f1e5d9a6a2f278e049b38a34d05bb
Lucas Combs <l-combs@hotmail.com>
2018-03-26 16:21:47 -0400

    Fixed off-by-one

--- __main__.py
+++ __main__.py
@@ -24,7 +24,7 @@ def gen_rooms():
 # Prompt user for name, generate random armor, weapon, & health
 def gen_hero():
     global player
-    player = Hero(input("Enter hero name:"))
+    player = Hero(input("Enter hero name: "))
 
 
 # Instantiate any variables needed
@@ -38,11 +38,11 @@ def init():
     random.seed(None)
 
     # Setup defaults for simple vars
-    current_room = 1 # Player starts out in room 1
+    current_room = 0 # Player starts out in room 0
 
 
 greeting()
 init()
-while current_room > 0:
+while current_room >= 0:
     current_room = rooms[current_room].prompt(player)
 print("\nGAME OVER")
